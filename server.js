d5574360351e12ddd6f1de3631b2d26a3d4763e4
andreduarte99 <andre.duarte16@outlook.com>
2024-10-07 13:29:34 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+//import http from "http";
+import app from "./src/app.js";
+
+const PORT = 3000;
+
+
+app.listen(PORT, () => {
+    console.log("servidor escutando!");
+});
+
+
+// const rotas = {
+//     "/": "Curso de Express API",
+//     "/livros": "Entrei na rota livros",
+//     "/autores": "Entrei na rota autores"
+// };
+
+// const server = http.createServer((req,res) =>{
+//     res.writeHead(200, {"Conteny-Type": "text/plain"});
+//     res.end(rotas[req.url]);
+// });
+
+// server.listen(PORT, () => {
+//     console.log("servidor escutando!");
+// });
+
+
+
