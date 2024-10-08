9cdd7a20e2541864ae2c2c18d102d4cacf801372
andreduarte99 <andre.duarte16@outlook.com>
2024-10-08 16:04:58 -0300

    persistindo dados finalizado

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("Erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco feita com sucesso.");
+});
 
 const app = express();
 
 app.use(express.json());
 
+/*
 const livros = [
     {
         id: 1,
@@ -20,13 +33,15 @@ function buscaLivro(id){
         return livro.id === Number(id);
     })
 }
+*/
 
 app.get("/", (req, res) =>{
     res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req, res) =>{
-    res.status(200).json(livros);
+app.get("/livros", async (req, res) =>{
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {
@@ -52,3 +67,4 @@ app.delete("/livros/:id", (req, res) =>{
 });
 
 export default app;
+
